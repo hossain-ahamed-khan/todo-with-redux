c8edcc6b6900d596f479f3d8d4ff007f2ae78e22
hossain <hossainahamedk@gmail.com>
2025-01-03 17:04:22 +0600

    added dark mode

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { ModeToggle } from "../mode-toggle";
 
 export default function Navbar() {
     return (
-        <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+        <nav className="max-w-screen-xl mx-auto h-16 flex items-center gap-3 px-5">
             <div>TODO WITH REDUX</div>
             <Link to="/">Tasks</Link>
             <Link to="/users">Users</Link>
             <Link to="/counter">Counter</Link>
+            <div className="ml-auto">
+                <ModeToggle />
+            </div>
         </nav>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { RouterProvider } from 'react-router-dom'
 import routes from './routes/route.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={routes} />
-    </Provider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
